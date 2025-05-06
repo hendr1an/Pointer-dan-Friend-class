b44c00c4a01208b68e4ecd197430ac634b58687b
hendr1an <hendriansyahgyan@gmail.com>
2025-05-06 10:32:08 +0700

    first commit

---
+++ contoh.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class PersegiPanjang
+{
+public:
+    int panjang, lebar;
+
+public:
+
+    void input()
+    {
+        cout << "Masukkan Panjang : ";
+        cin >> panjang;
+        cout << "Masukkan Lebar : ";
+        cin >> lebar;
+    }
+}
